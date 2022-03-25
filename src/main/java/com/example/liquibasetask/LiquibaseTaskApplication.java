35d6483cfa5d3f9d4a18354d29e7cde32902ffa1
Raman Rashchupkin <rrashchupkin@piesoft.us>
2022-03-25 16:13:10 +0300

    Initial commit

---
+++ src/main/java/com/example/liquibasetask/LiquibaseTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibasetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseTaskApplication.class, args);
+    }
+
+}
